20dc62be5578a237a71cb1d68596cf067a69c7c0
jeffyuyi <442348584@qq.com>
2026-01-18 12:46:42 +0800

    修复路径配置

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/DND5R-easy-cc/', // GitHub Pages 部署路径
+  base: '/---dnd5r------/', // GitHub Pages 部署路径（旧仓库）
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
