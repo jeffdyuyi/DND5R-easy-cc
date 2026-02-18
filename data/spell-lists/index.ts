43e57c944345318c25b92cb74e31d5e50f66b65d
jeffyuyi <442348584@qq.com>
2026-02-18 16:02:56 +0800

    feat: Introduce Warlock class and spell list, and establish a centralized spell list database.

--- data/spell-lists/index.ts
+++ data/spell-lists/index.ts
@@ -1,14 +1,17 @@
 import { RANGER_SPELLS } from './ranger';
 import { SORCERER_SPELLS } from './sorcerer';
+import { WARLOCK_SPELLS } from './warlock';
 
 // Mapping of class name (Chinese) to their spell list
 export const SPELL_LISTS_DB: Record<string, string[]> = {
     "游侠": RANGER_SPELLS,
     "术士": SORCERER_SPELLS,
+    "魔契师": WARLOCK_SPELLS,
     // Add other classes here as their lists are created
 };
 
 export {
     RANGER_SPELLS,
-    SORCERER_SPELLS
+    SORCERER_SPELLS,
+    WARLOCK_SPELLS
 };
